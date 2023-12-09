a9ee6028a71f9ef3b45803b6f3aa784acb8649bc
elham <elham.es.haghi.qomi@gmail.com>
2023-12-10 00:12:13 +0330

    last commit before update ang&.net

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { AccountService } from 'src/app/services/account.service';
 
 
 @Component({
@@ -6,5 +8,18 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  user: User | null | undefined;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.accountService.currentUser$.subscribe({
+      next: res => this.user = res
+    });
+  }
+
+  logout(): void {
+    this.accountService.logoutUser();
+  }
 }
